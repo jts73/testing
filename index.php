50f527caf1ba65d5e24a074375057aef5b323a97
Justin Schneider <justin.schneider91@gmail.com>
2012-11-20 22:32:30 -0700

    structure

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="author" content="Justin Schneider" />
+	<title></title>
+</head>
+<body>
+
+	<h1>My Site</h1>
+
+</body>
+</html>
